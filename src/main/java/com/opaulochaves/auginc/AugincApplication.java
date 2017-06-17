b181bdee0d2cecb0a84eb156d8cf89922ef8eca3
Paulo Chaves <opaulochaves@gmail.com>
2017-06-16 22:44:11 -0300

    Initial commit

---
+++ src/main/java/com/opaulochaves/auginc/AugincApplication.java
@@ -0,0 +1,12 @@
+package com.opaulochaves.auginc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugincApplication.class, args);
+	}
+}
