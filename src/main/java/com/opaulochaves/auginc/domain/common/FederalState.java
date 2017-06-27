9211ba3fb68624a6c00bc5bfc67e3b484c2f1ab4
Paulo Chaves <opaulochaves@gmail.com>
2017-06-27 13:38:16 -0300

    add enum and controller to list federal states

---
+++ src/main/java/com/opaulochaves/auginc/domain/common/FederalState.java
@@ -0,0 +1,30 @@
+package com.opaulochaves.auginc.domain.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum FederalState {
+
+    AC("Acre"), AL("Alagoas"), AP("Amapá"), AM("Amazonas"),
+    BA("Bahia"), CE("Ceará"), DF("Distrito Federal"), ES("Espírito Santo"),
+    GO("Goiás"), MA("Maranhão"), MT("Mato Grosso"), MS("Mato Grosso do Sul"),
+    MG("Minas Gerais"), PA("Pará"), PB("Paraíba"), PR("Paraná"),
+    PE("Pernambuco"), PI("Piauí"), RJ("Rio de Janeiro"),
+    RN("Rio Grande do Norte"), RS("Rio Grande do Sul"), RO("Rondônia"),
+    RR("Roraima"), SC("Santa Catarina"), SP("São Paulo"), SE("Sergipe"),
+    TO("Tocantins");
+
+    private String state;
+
+    FederalState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getName() {
+        return this.toString();
+    }
+}
---
+++ src/main/java/com/opaulochaves/auginc/domain/common/FederalStateController.java
@@ -0,0 +1,20 @@
+package com.opaulochaves.auginc.domain.common;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/federalStates")
+public class FederalStateController {
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public List<FederalState> federalStates() {
+        return Arrays.asList(FederalState.values());
+    }
+}
